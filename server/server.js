8a2f9390f02175d2137a118a600d7a86abd81abd
Haddad <ahmedabduelrahmen.haddad@esprit.tn>
2022-10-21 14:35:51 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use('/offres', require('./modules/offres/offre.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 443;
 app.listen(port, () => console.log('Server listening on port ' + port));
 
 // //LocalTunnel
